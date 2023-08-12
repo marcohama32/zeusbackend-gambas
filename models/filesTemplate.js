82f665b6e67cb031755c016167e3d7869f92a1cf
marcohama32 <50961103+marcohama32@users.noreply.github.com>
2023-08-12 16:21:09 +0200

    Add files via upload
    Add files Template

---
+++ models/filesTemplate.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const FilesTemplateSchema = new mongoose.Schema(
+  {
+    fileTemplate: {
+      type: String,
+      required: [true, "Avatar is required"],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "Description is required"],
+    },
+    user: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("FilesTemplate", FilesTemplateSchema);
