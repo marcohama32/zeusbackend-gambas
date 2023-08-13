1a7f3448e35a854907a3337fb7b23e75418b4590
marcohama32 <50961103+marcohama32@users.noreply.github.com>
2023-08-13 04:58:43 +0200

    Update customertransactionController.js
    up

--- controllers/customertransactionController.js
+++ controllers/customertransactionController.js
@@ -1310,63 +1310,55 @@ exports.ussd = asyncHandler(async (req, res, next) => {
           select: "serviceName remainingBalance",
         },
       });
-
+  
       if (result && result.plan) {
         const plan = result.plan;
         const pageSize = 5;
-        let totalServicesDisplayed = 0;
-
+  
+        let response = `CON Your Plan Services:\n`;
+  
         // Get the index of the last displayed service from sessionState
         let lastServiceIndex = sessionState.lastServiceIndex || 0;
-
-        response = `CON Your Plan Services:\n`;
-
+  
         for (let planIndex = 0; planIndex < plan.length; planIndex++) {
           const planService = plan[planIndex].planService;
-
-          for (
-            let serviceIndex = lastServiceIndex;
-            serviceIndex < planService.length;
-            serviceIndex++
-          ) {
+  
+          for (let serviceIndex = lastServiceIndex; serviceIndex < planService.length; serviceIndex++) {
             const service = planService[serviceIndex];
-            const serviceNumber = totalServicesDisplayed + 1;
-
+            const serviceNumber = serviceIndex + 1;
+  
             response += `${serviceNumber}. Benefit: ${service.serviceName}\n`;
             response += `   Balance: ${service.remainingBalance}\n\n`;
-
-            totalServicesDisplayed++;
-            lastServiceIndex = serviceIndex;
-
-            if (totalServicesDisplayed >= pageSize) {
+  
+            if (serviceNumber % pageSize === 0 && serviceNumber < planService.length) {
               // Limit reached, provide option to show more
               response += `99. Show more\n`;
+              lastServiceIndex = serviceIndex + 1;
               break;
             }
           }
-
-          if (totalServicesDisplayed >= pageSize) {
-            break;
-          }
         }
-
-        if (lastServiceIndex < plan[plan.length - 1].planService.length) {
-          // There are more services to display, set the lastServiceIndex for the next session
-          sessionState.lastServiceIndex = lastServiceIndex + 1;
-        } else {
+  
+        if (lastServiceIndex >= plan[plan.length - 1].planService.length) {
           // All services have been displayed
-          sessionState.lastServiceIndex = null;
+          response = `END No more Plan Services to show`;
         }
-
-        if (totalServicesDisplayed === 0) {
-          response = `END No Plan Services found for your number`;
-        }
+  
+        // Update sessionState with the lastServiceIndex
+        sessionState.lastServiceIndex = lastServiceIndex;
+  
+        // Send the response back to API
+        res.set('Content-type: text/plain');
+        res.send(response);
+  
       } else {
         response = `END Plan Services not found for your number`;
       }
     } catch (error) {
       console.error("Error:", error);
       response = `END Error fetching Plan Benefits`;
+      res.set('Content-type: text/plain');
+      res.send(response);
     }
   } else if (text === "3") {
     // Terminal response
